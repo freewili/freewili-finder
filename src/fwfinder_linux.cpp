ce90637878ef4f305fcb45bfca3e973005eccab8
David Rebbe <drebbe@intrepidcs.com>
2025-04-07 11:21:15 -0400

    compiler fixes for linux/mac

--- src/fwfinder_linux.cpp
+++ src/fwfinder_linux.cpp
@@ -2,6 +2,9 @@
 
     #include <fwfinder.hpp>
 
+    #include <expected>
+    #include <string>
+
 auto Fw::find_all() noexcept
     -> std::expected<Fw::FreeWiliDevices, std::string> {
     return std::unexpected("TODO");
